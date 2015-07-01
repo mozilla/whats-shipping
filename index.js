53bc3fcfffe1b24a13149e425e3393db47c79d24
Matt Claypotch <thepotch@gmail.com>
2015-06-30 17:16:12 -0700

    CORS to the max

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ app.set('port', (process.env.PORT || 8000));
 // app.use(express.static(__dirname + '/static'));
 var server = http.createServer(app);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
   res.json(channels);
 });
