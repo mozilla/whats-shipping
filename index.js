cb60c123c10297923fe3cb86259074b6035987a5
Matt Claypotch <thepotch@gmail.com>
2015-12-15 14:34:56 -0800

    added dashboard

--- index.js
+++ index.js
@@ -52,6 +52,11 @@ app.get('/', function(req, res) {
   res.json(channels);
 });
 
+var fs = require('fs');
+app.get('/dashboard', function(req, res) {
+  res.end(fs.readFileSync('./dashboard.html'));
+});
+
 server.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
